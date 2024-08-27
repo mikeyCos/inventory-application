550890b02c3cc4b3c9582d6ade39b9a738fd4437
Michael <109037087+mikeyCos@users.noreply.github.com>
2024-08-27 16:57:46 -0700

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+---
+### 26 AUG 2024
+- Initialized `NodeJS`/`Express` boilerplate template named `node-express-template`.
+- Created a variety of files including: `CHANGELOG.md`, `app.js`, and `PROJECT_SPECIFICATIONS`.
+- Created a variety of subdirectories including: `paths`, `public`, `routes`, `utils`, and `views`.
+- Template can be locally started with `npm run dev`.
+- Defined static and non-static paths in `paths/paths.js`.
+- Created and linked a `reset` stylesheet.
+---
---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { PORT } = require("./utils/environment");
+const { staticPaths, viewsPaths } = require("./paths/paths");
+const indexRouter = require("./routes/indexRouter");
+const placeholderRouter = require("./routes/placeholderRouter");
+
+const app = express();
+
+// Specify static paths
+app.use(staticPaths.map((path) => express.static(path)));
+
+// Setting views
+app.set("views", viewsPaths);
+app.set("view engine", "ejs");
+
+// Parses form data
+app.use(express.urlencoded({ extended: true }));
+
+// Specify routes
+app.use("/", indexRouter);
+app.use("/placeholderA", placeholderRouter);
+
+app.use((req, res) => {
+  res.render("404", { title: "404 - Page Not Found" });
+});
+
+// Error middleware function
+app.use((err, req, res, next) => {
+  console.log("error middleware running...");
+});
+
+app.listen(PORT, () => console.log(`Application running on port: ${PORT}`));
